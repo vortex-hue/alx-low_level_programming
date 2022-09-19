d8cb5543acd804656a125b4727f1e2971b3e15b7
Ani peter benjamin <99623072+vortex-hue@users.noreply.github.com>
2022-09-19 14:32:02 +0100

    updated 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,14 @@
 #include "main.h"
 
 /**
- * _puts - prints string
- * @str: input string
- * Return: no return
+ * _puts - write string
+ * @str: string
+ * Return: void
  */
-
 void _puts(char *str)
 {
-	while (*str != '\0')
-	{
-		_putchar(*str);
-		str++;
-	}
+	while (*str)
+		_putchar(*str++);
+
 	_putchar('\n');
 }
