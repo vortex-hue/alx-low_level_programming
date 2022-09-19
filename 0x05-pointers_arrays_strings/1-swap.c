919c7c75d387c2b024ce97e08888a6b4aa0cfc7a
Ani peter <benjaminparish6@gmail.com>
2022-09-19 16:31:06 +0100

    added swap_int

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swap values of two nums
+ *@b : first pointer
+ *@a : Second pointer
+ * Return: void or nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+    int constant = *a;
+    *b = *a;
+    *a = constant;
+}
