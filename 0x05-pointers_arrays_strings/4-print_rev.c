735d7957bdddf98d092e5098f2d43a6211fb9afb
Ani peter benjamin <99623072+vortex-hue@users.noreply.github.com>
2022-09-19 14:46:38 +0100

    included string.h

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,20 +1,17 @@
-#include main.h
+#include "main.h"
+#include <string.h>
 
 /**
-*print_rev : writes str in reverse order
-* @s : a pointer
-* Return: void
-*/
-
+ * print_rev - string in reve
+ * @s: string
+ */
 void print_rev(char *s)
 {
-  int len = 0, index = 0;
+	int i, len;
+	len = strlen(s);
 
-	while (s[index++])
-		len++;
-
-	for (index = len - 1; index > 0; index--)
-		_putchar(s[index]);
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
 
 	_putchar('\n');
 }
