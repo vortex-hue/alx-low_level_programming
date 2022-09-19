ffa98441e588c7f149d6854857657a68524d83d2
Ani peter <benjaminparish6@gmail.com>
2022-09-19 12:46:53 +0100

    betty

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,11 +9,12 @@
 
 void _puts(char *str)
 {
-int i = 0;
-while (str[i])
-{
-	_putchar(str[i]);
-	i++;
-}
-_putchar('\n');
+    int i = 0;
+
+    while (str[i] != '\0')
+    {
+	    _putchar(str[i]);
+	    i++;
+    }
+    _putchar('\n');
 }
