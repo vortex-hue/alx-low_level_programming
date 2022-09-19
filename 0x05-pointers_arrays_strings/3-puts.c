5abfcdf522f68d9adbf4dc6586ce76aa61398ed4
Ani peter <benjaminparish6@gmail.com>
2022-09-19 12:54:24 +0100

    updated error

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,15 +6,13 @@
  * Return: void
 */
 
-
 void _puts(char *str)
 {
-    int i = 0;
+	int i;
 
-    while (str[i] != '\0')
-    {
-	    _putchar(str[i]);
-	    i++;
-    }
-    _putchar('\n');
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
