bff448cae2ad2bccb780d8ef7c8b48951f5f84cf
Ani peter <benjaminparish6@gmail.com>
2022-09-19 10:39:15 +0100

    created a c file that swap int

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *swap_int - swaps the value of two integer
+ *@a : pointer
+ *@b : pointer
+ * Return : void
+ */
+
+void swap_int(int *a, int *b)
+{
+    b = *a;
+    a = *b;
+}
