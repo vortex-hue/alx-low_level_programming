b8a71773b4cce6da780bbe06eb96b661c119c60f
Ani peter <benjaminparish6@gmail.com>
2022-09-19 10:24:02 +0100

    created first file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - reset the value of n to 98
+ *
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+*This includes all the prototypes
+*
+*/
+
+void reset_to_98(int *n);
+
+
+
+
+
+#endif /* MAIN_H */
