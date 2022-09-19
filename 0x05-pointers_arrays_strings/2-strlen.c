ce532be89c70543170b9fe6e89250532eec053e2
Ani peter <benjaminparish6@gmail.com>
2022-09-19 17:05:36 +0100

    betty linter

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -10,9 +10,9 @@ int _strlen(char *s)
 {
     int length = 0;
 
-    while(s[length] != '\0')
+    while (s[length] != '\0')
     {
-        length++;
+            length++;
     }
 
     return (length);
