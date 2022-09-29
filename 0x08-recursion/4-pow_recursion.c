4e53f3726db540736602fa00a20c7c72b6dad33d
Ani peter benjamin <99623072+vortex-hue@users.noreply.github.com>
2022-09-29 23:10:27 +0100

    fixed betty

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _pow_recursion - a function that returns the value of x raised to the power of y
+ * _pow_recursion - a function that returns
+ * the value of x raised to the power of y
  * @x: integer params
  * @y: integer paramtr
  * Return: recursion
