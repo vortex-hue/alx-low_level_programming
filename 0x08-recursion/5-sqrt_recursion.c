d25b5257bae5c8412586a07b9e9682d1e0d87563
Ani peter benjamin <99623072+vortex-hue@users.noreply.github.com>
2022-09-29 23:09:59 +0100

    fixed betty

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - a function that returns the natural square root of a number.
+ * _sqrt_recursion - a function that returns
+ * the natural square root of a number.
  * @n: int n
  * Return: int
  */
