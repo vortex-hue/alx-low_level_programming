9e16c7dce89abb7c63d21aae9c7b72729d96e739
Ani peter benjamin <99623072+vortex-hue@users.noreply.github.com>
2022-09-29 22:59:40 +0100

    added 101-wildcmp.c file

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * wildcmp -  a function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0.
+ * @s1: pointer to string params
+ * @s2: pointer to string params
+ * Return: 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
