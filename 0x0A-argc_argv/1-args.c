4e1ee6ff3e3287e6daf58eaa7ff4df26ed9c2821
Ani peter benjamin <99623072+vortex-hue@users.noreply.github.com>
2022-10-01 22:25:48 +0100

    fixed checker

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
- * main -  a program that prints the 
- * number of arguments passed into it.
+ * main - prints the number of arguments passed into it.
  * @argc: number of command line arguments.
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
-
 int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
