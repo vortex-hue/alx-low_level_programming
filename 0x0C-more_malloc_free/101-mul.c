c0bd2525eda0fb7b7b1b27120395a1f6502afafe
Ani peter benjamin <99623072+vortex-hue@users.noreply.github.com>
2022-10-07 05:09:21 +0100

    betty

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -6,13 +6,19 @@
  * @argv: args
  * Return: int
  */
+
+
 int main(int argc, char *argv[])
 {
-unsigned long mul;
-int i, j;
+	unsigned long mul;
+	int i, j;
+		
 	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
+	{ 
+		printf("Error\n");
+	 	exit(98); 
+	}
+	
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
@@ -25,5 +31,6 @@ int i, j;
 	}
 	mul = atol(argv[1]) *  atol(argv[2]);
 	printf("%lu\n", mul);
+	
 return (0);
 }
